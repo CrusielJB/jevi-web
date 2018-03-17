1a7d7a58f18fdfd684fe7cd6f0717482448363df
Crusiel baez <Crusiel jb>
2018-03-16 22:12:43 -0400

    guardado

---
+++ src/app/Registro/Registro.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app-Registro',
+  templateUrl: './Registro.component.html',
+  styleUrls: ['./Registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navbar/Navbar.component';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegistroComponent } from './Registro/Registro.component';
+
+const appRouters: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent},
+  {path: 'Registro', component: RegistroComponent}
+  ];
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    IndexComponent,
+    FooterComponent,
+    RegistroComponent
+],
   imports: [
+    RouterModule.forRoot(appRouters),
     BrowserModule
   ],
   providers: [],
